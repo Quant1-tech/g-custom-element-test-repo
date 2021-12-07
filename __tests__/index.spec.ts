172d21d6bf46119b1b2e5be71c081d72cf6d26d4
yuqi.pyq <yuqi.pyq@antgroup.com>
2021-12-07 22:08:35 +0800

    init

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,49 @@
-import { version } from '../src';
+import { Canvas, Circle, Group } from '@antv/g';
+import { Renderer } from '@antv/g-canvas';
+import { MyCustomShape } from '../src';
 
 describe('template', () => {
   test('export', () => {
-    expect(version).toBe('0.1.0');
+    const $container = document.createElement('div');
+    $container.id = 'container';
+    document.body.prepend($container);
+
+    // create a renderer
+    const renderer = new Renderer();
+
+    // create a canvas
+    const canvas = new Canvas({
+      container: 'container',
+      width: 600,
+      height: 500,
+      renderer,
+    });
+
+    const group = new Group();
+
+    const circle = new Circle({
+      style: {
+        x: 10,
+        y: 10,
+        r: 20,
+        fill: 'white',
+      }
+    });
+
+    const myCustomShape = new MyCustomShape({
+      style: {
+        prop1: 'test',
+      },
+    });
+    
+    group.appendChild(circle);
+    group.appendChild(myCustomShape);
+    canvas.appendChild(group);
+
+    // remove 2s later
+    setTimeout(() => {
+      myCustomShape.style.prop1 = 'test2';
+      group.removeChild(myCustomShape);
+    }, 2000);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-export const version = '0.1.0';
+import { CustomElement, Circle, DisplayObjectConfig } from '@antv/g';
+
+interface MyCustomShapeProps {
+    prop1: string;
+}
+
+export class MyCustomShape extends CustomElement<MyCustomShapeProps> {
+    constructor(config: DisplayObjectConfig<MyCustomShapeProps>) {
+        super({
+            ...config,
+            type: 'test',
+        });
+
+        this.style.prop1;
+
+        this.appendChild(new Circle({
+            style: {
+                x: 100,
+                y: 100,
+                r: 100,
+                fill: 'red',
+            }
+        }));
+    }
+
+    connectedCallback() {
+        console.log('disconnected');
+    }
+
+    disconnectedCallback() {
+        console.log('disconnected');
+    }
+
+    attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+        console.log('attribute changed', name, oldValue, newValue);
+    }
+}
